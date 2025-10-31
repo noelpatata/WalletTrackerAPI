87bf1ebbe1b0db79da073267f95b15f9997533b9
Noel <novocorboudnoel@gmail.com>
2025-10-31 18:08:28 +0100

    default values for env config.py, renamed env variables in pipeline

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 
-DATABASE_PASSWORD = os.environ["DATABASE_ROOT_PASSWORD"]
-DATABASE_NAME = os.environ["DATABASE_NAME"]
-DATABASE_USERNAME = os.environ["WALLET_TRACKER_DB_USER"]
-DATABASE_HOST = os.environ["WALLET_TRACKER_DB_HOST"]
-SECRET = os.environ["WALLET_TRACKER_SECRET"]
+DATABASE_PASSWORD = os.environ.get("DATABASE_ROOT_PASSWORD", "12345678")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "wallet_tracker")
+DATABASE_USERNAME = os.environ.get("WALLET_TRACKER_DB_USER", "root")
+DATABASE_HOST = os.environ.get("WALLET_TRACKER_DB_HOST", "db")
+SECRET = os.environ.get("WALLET_TRACKER_SECRET", "randomSecret")
 ENABLE_REGISTER = os.environ.get("ENABLE_REGISTER", "false")
