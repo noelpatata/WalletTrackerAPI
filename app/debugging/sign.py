c457e5ff16d7f052432cba0644bb2448fcc4cb7b
noelpatata <noel>
2026-03-08 16:33:03 +0100

    delete on cascade

--- app/debugging/sign.py
+++ app/debugging/sign.py
@@ -1,11 +1,10 @@
 import base64
-from config import SECRET
 from utils.Cryptography import sign
 
-with open("testing/private_key.pem", "rb") as f:
+with open("debugging/testing/private_key.pem", "rb") as f:
     private_key_bytes = f.read()
 
 private_key_pem = base64.b64encode(private_key_bytes)
 
-signature = sign(SECRET, private_key_pem)
+signature = sign(private_key_pem)
 print("Here's your signature:\n"+signature)
